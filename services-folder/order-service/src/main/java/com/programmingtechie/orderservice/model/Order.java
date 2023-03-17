fa469222cc3425ba776e71a87fcbbb82f1d3fcbc
Denny Wibowo <indocoffee@gmail.com>
2023-03-17 16:25:35 -0400

    Add OneToMany relationship

--- services-folder/order-service/src/main/java/com/programmingtechie/orderservice/model/Order.java
+++ services-folder/order-service/src/main/java/com/programmingtechie/orderservice/model/Order.java
@@ -20,5 +20,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
     private List<OrderLineItems> orderLineItemsList;
 }
