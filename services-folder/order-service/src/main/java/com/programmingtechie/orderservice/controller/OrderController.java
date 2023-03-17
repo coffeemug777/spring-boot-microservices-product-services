fdfb91629d4e423db95b5add994e026712034f42
Denny Wibowo <indocoffee@gmail.com>
2023-03-17 16:44:32 -0400

    Call service from controller

--- services-folder/order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ services-folder/order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -1,17 +1,21 @@
 package com.programmingtechie.orderservice.controller;
 
 import com.programmingtechie.orderservice.dto.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.programmingtechie.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        this.orderService.placeOrder(orderRequest);
         return "order place success";
     }
 
